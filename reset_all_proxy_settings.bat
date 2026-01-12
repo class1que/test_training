d736b52fc743e983e46b066bac9feea4a58d5330
class1que <class1que.l92@gmail.com>
2026-01-12 16:37:49 +0300

    Update reset_all_proxy_settings.bat

--- reset_all_proxy_settings.bat
+++ reset_all_proxy_settings.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 1251 >nul
 
-echo Сбрасываю настройки прокси...
+echo РЎР±СЂР°СЃС‹РІР°СЋ РЅР°СЃС‚СЂРѕР№РєРё РїСЂРѕРєСЃРё...
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "" /f
@@ -11,6 +11,7 @@ net stop WinHttpAutoProxySvc >nul 2>&1
 net start WinHttpAutoProxySvc >nul 2>&1
 
 ipconfig /flushdns >nul
+netsh winsock reset
 
-echo Готово. Закройте окно нажав на "Крестик" !
+echo Р“РѕС‚РѕРІРѕ!
 pause
